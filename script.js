9a3fa2155f6224c1fae3c67c67a4c29d7cfaf6bd
Николай <derphonix98@mail.ru>
2023-08-06 15:09:24 +0600

    feat: task-5 done!

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-alert("Do you want to enter a title for a block?");
-const commandForCurrentUser = prompt("Enter the title for a block");
-const secondCommandForCurrentUser = prompt("Enter unit's title");
-const dataFromCurrentUser = commandForCurrentUser + secondCommandForCurrentUser;
-if (dataFromCurrentUser) {
-    alert(`${commandForCurrentUser}, ${secondCommandForCurrentUser}`);
-} 
+const firstNumber = +prompt("Enter the first number");
+const secondNumber = +prompt("Enter the second number");
+const operator = prompt("Enter +, -, *, /");
+function calcResult(firstNumber, secondNumber, operator) {
+    return eval(`${firstNumber}` + operator + `${secondNumber}`);
+}
+alert(calcResult(firstNumber, secondNumber, operator));
