11e98e543f6af7d19adc56527dff215468f0b4dc
Николай <derphonix98@mail.ru>
2023-08-06 13:21:40 +0600

    feat: task-2 done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const userName = prompt("Enter your name");
-if (userName) {
-    alert (userName);
+const numberOne = prompt ("Enter the first number");
+const numberTwo = prompt ("Enter the second number");
+const sumOfNumbers = +numberOne + +numberTwo;
+if (numberOne, numberTwo) {
+    alert (sumOfNumbers);
 } else {
-    alert ("You didn't enter the name");
+    alert ("You didn't enter the numbers");
 }
